f0f50ccb7c55894fdd884bc8fdac427d599f9157
Anura1978 <anuraabenayaka6@gmail.com>
2026-08-02 10:37:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-script.js
+function calculateFare() {
+
+    let distance = document.getElementById("distance").value;
+
+    if (distance === "") {
+        alert("Please enter distance");
+        return;
+    }
+
+    let fare = distance * 120;
+
+    document.getElementById("fare").innerHTML =
+        "Estimated Fare: LKR " + fare;
